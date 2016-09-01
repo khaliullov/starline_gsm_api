83a225a9c4f2b6ee43ba150ee2fe1178b04c8e79
Leandr Khaliullov <mail@leandr.su>
2016-09-01 19:35:21 +0000

    more detailed example of using

--- client.py
+++ client.py
@@ -21,22 +21,88 @@ request = {
 }
 
 
-def main(event):
-    aws = http.client.HTTPSConnection("xu3g4ixx6c.execute-api.eu-central-1.amazonaws.com")
-
+def _ajax_request(host, method, path, params=None, _headers={}):
+    handle = http.client.HTTPSConnection(host)
     headers = {"Content-type": "application/json; charset=utf-8",
                "User-Agent": "Mozilla/5.0",
                "Accept": "application/json"}
-    params = json.dumps(event)
-    aws.request("POST", "/starline/get_token", params, headers)
-    res = aws.getresponse()
+    headers.update(_headers)
+
+    handle.request(method, path, params, headers)
+    res = handle.getresponse()
     if res.status != 200:
         response['codestring'] = 'Failed to auth'
         response['code'] = res.status
         return response
-    return res.read()
+    return json.loads(res.read().decode('utf-8'))
 
 
-res = main(request)
+def get_credentials(req):
+    credentials = _ajax_request('xu3g4ixx6c.execute-api.eu-central-1.amazonaws.com',
+                                'POST', '/starline/get_token', json.dumps(req))
 
-pprint.pprint(res)
+    return credentials
+
+
+def get_devices(credentials):
+    devices = _ajax_request('dev.starline.ru', 'GET', '/json/user/' +
+                            credentials['user_id'] + '/devices', None,
+                            {"Cookie": credentials['cookie']})
+    return devices
+
+
+def get_state(credentials, device_id):
+    state = _ajax_request('dev.starline.ru', 'GET', '/json/device/' +
+                          device_id, None,
+                          {"Cookie": credentials['cookie']})
+    return state
+
+def set_state(credentials, device_id, state={}):
+    new_state = _ajax_request('dev.starline.ru', 'POST', '/json/device/' +
+                              device_id + '/set_param', json.dumps(state),
+                             {"Cookie": credentials['cookie']})
+    return new_state
+
+
+def main():
+    result = get_credentials(request)
+    pprint.pprint(result)
+    if 'code' not in result or int(result['code']) != 200:
+        pprint.pprint(result)
+        return 1
+
+    credentials = result
+
+    result = get_devices(credentials)
+    if int(result['code']) != 200:
+        pprint.pprint(result)
+        return 2
+
+    devices = result['devices']
+
+    result = get_state(credentials, devices[0]['device_id'])
+
+    if int(result['code']) != 200:
+        pprint.pprint(result)
+        return 3
+
+    state = result
+
+    print("Devices:")
+    pprint.pprint(devices)
+    print("Device[0] state:")
+    pprint.pprint(state['device']['state'])
+
+    # disalarm
+    # result = set_state(credentials, devices[0]['device_id'], {'arm': 0})
+    # print("Disalarmed:")
+    # pprint.pprint(result)
+
+    # alarm
+    # result = set_state(credentials, devices[0]['device_id'], {'arm': 1})
+    # print("Alarmed:")
+    # pprint.pprint(result)
+
+
+if __name__== '__main__':
+    main()
